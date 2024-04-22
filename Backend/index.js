47055664951730d3bcf442f4094d238806b58756
Valentín Peñalver <valenpga@gmail.com>
2024-04-22 19:09:21 +0200

    Base de datos conectada

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 PORT = 3000
 
-
+dbconnect()
 
 app.listen(PORT, () => {
     console.log(`Express esta escuchando en el puerto http://localhost:${PORT}`)
