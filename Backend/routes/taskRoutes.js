6eacdc7fe8c721283d4bbb585fe369c1dbfb6181
Valentín Peñalver <valenpga@gmail.com>
2024-04-25 13:11:04 +0200

    Frontend

--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -10,6 +10,6 @@ router.get('/id/:_id', TaskController.getById)
 router.delete('/id/:_id',TaskController.deleteTask)
 router.put('/markascompleted/:_id', TaskController.updateCompleted)
 router.put('/markasincompleted/:_id', TaskController.updateIncomplete)
-router.put('/editTask', TaskController.editTask)
+router.put('/editTask/:_id', TaskController.editTask)
 
 module.exports = router;
