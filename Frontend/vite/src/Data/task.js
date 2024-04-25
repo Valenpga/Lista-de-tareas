9b74bb34ccb8a57c8baf02e611edc922f6f814c2
Valentín Peñalver <valenpga@gmail.com>
2024-04-25 23:04:27 +0200

    creando front

---
+++ Frontend/vite/src/Data/task.js
@@ -0,0 +1,19 @@
+export const task = [
+    {
+        id:0,
+        title:"mi primera tarea",
+        description:"mi primera tarea"
+    },
+
+    {
+        id:1,
+        title:"mi segunda tarea",
+        description:"mi segunda tarea"
+    },
+
+    {
+        id:2,
+        title:"mi segunda tarea",
+        description:"mi segunda tarea"
+    }
+]
